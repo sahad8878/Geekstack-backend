36230ab4f9169d57c55875ee65cbc316c623bf9a
sahad <msahadpp@gmail.com>
2023-09-02 01:33:46 +0530

    multer updated

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors';
-// import NodeMediaServer from 'node-media-server'
+import NodeMediaServer from 'node-media-server'
 import path from 'path'
 import { fileURLToPath } from 'url';
 import videoRouter from './routes/videoRoutes.js'
-// import userRouter from './routes/userRoutes.js'
+import userRouter from './routes/userRoutes.js'
 dotenv.config()
 
 import './config/connectDB.js'
-// import { errorHandler, notFound } from "./middlewares/errorMiddlwares.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddlwares.js";
   
 
 
@@ -22,34 +22,33 @@ app.use(express.json())
 //   };
   
   app.use(cors());
-  // const config = {
-  //   rtmp: {
-  //     port: 1935,
-  //     chunk_size: 60000,
-  //     gop_cache: true,
-  //     ping: 30,
-  //     ping_timeout: 60,
-  //   },
-  //   http: {
-  //     port: 8000,
-  //     mediaroot: './media',
-  //     allow_origin: '*',
-  //   },
-  // };
+  const config = {
+    rtmp: {
+      port: 1935,
+      chunk_size: 60000,
+      gop_cache: true,
+      ping: 30,
+      ping_timeout: 60,
+    },
+    http: {
+      port: 8000,
+      mediaroot: './media',
+      allow_origin: '*',
+    },
+  };
   
-  // const nms = new NodeMediaServer(config);
-  // nms.run();
+  const nms = new NodeMediaServer(config);
+  nms.run();
 
-  // app.use("/api/user",userRouter)
+  app.use("/api/user",userRouter)
   app.use("/api/videos",videoRouter)
 
-//   app.use(notFound);
-// app.use(errorHandler);
+  app.use(notFound);
+app.use(errorHandler);
 
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  // console.log(__dirname);
-  // // app.use('/public',express.static(path.join(__dirname,'public')))
+
   app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
